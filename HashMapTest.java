0c5c369a5697c29073873b0af382740921f9bb29
Ranjithgc <ranjith.amca.16@acharya.ac.in>
2021-06-08 18:54:25 +0530

    Adding Test Cases

--- HashMapTest.java
+++ HashMapTest.java
@@ -25,4 +25,24 @@ public class MyHashMapTest {
         	Assert.assertEquals(2, frequency);
 	}
 
+	@Test
+    	public void givenSentence_whenAddedToList_shouldReturnWordFrequency() {
+        	String sentence = "“Paranoids are not paranoid because they are paranoid but
+				  because they keep putting themselves deliberately into paranoid 				  avoidable situations”";
+        	MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+        	String[] words = sentence.toLowerCase().split(" ");
+        	for(String word : words) {
+            		Integer value = myHashMap.get(word);
+            		if (value == null) {
+                		value = 1;
+            		} else {
+                		value = value + 1;
+			}
+            		myHashMap.add(word, value);
+        	}
+        	int frequency = myHashMap.get("paranoid");
+        	System.out.println(myHashMap);
+        	Assert.assertEquals(2, frequency);
+	}
+
 }
